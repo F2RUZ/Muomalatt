bdc1c39eaa767bf344062ec0f29901fb1b2b0bbf
Feruz <119690313+F2RUZ@users.noreply.github.com>
2024-11-18 06:31:51 -0600

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./footer.scss";
 import { NavLink } from "react-router-dom";
 import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
-import logo from "../../assets/img/logo.png";
+import logo from "../../assets/logo.png";
 
 const Footer = () => {
   return (
